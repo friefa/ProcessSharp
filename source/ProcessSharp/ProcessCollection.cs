b0fd78b1307f191b9571049f9e9019c92200fd9d
Fabian Friedl <friedlfabian@gmx.de>
2020-06-18 22:49:49 +0200

    Added process collection
    Provides all Processes on a machine

---
+++ source/ProcessSharp/ProcessCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProcessSharp
+{
+    public class ProcessCollection
+    {
+        public Process[] Processes { get; private set; }
+
+        public DateTime CreationTimeUtc { get; private set; }
+
+        public static ProcessCollection OpenLocalProcesses()
+        {
+            ProcessCollection collection = new ProcessCollection();
+
+            System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcesses();
+            Process[] _processes = new Process[processes.Length];
+
+            for (int i = 0; i < processes.Length; i++)
+            {
+                _processes[i] = Process.Open(processes[i]);
+            }
+
+            collection.Processes = _processes;
+            collection.CreationTimeUtc = DateTime.UtcNow;
+
+            return collection;
+        }
+    }
+}
